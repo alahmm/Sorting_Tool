040790839709a313b6310b99aed45b09485dfc70
alahmm <Meriem.Al.Ahmadi.Hammou@msg.group>
2023-06-03 23:17:50 +0200

    sorting the given integers using Insertion sort and giving them back

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,41 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void Sorter (Scanner scanner) {
+        List<Integer> list = new ArrayList<>();
+        while (scanner.hasNext()) {
+            list.add(scanner.nextInt());
+        }
+        System.out.printf("%nTotal numbers: %d.", list.size());
+        int i = 1;
+        int j;
+        int x;
+        int[] array = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            array[k] = list.get(k);
+
+        }
+        //Arrays.sort(array);
+        while (i < list.size()) {
+            x = array[i];
+            j = i - 1;
+            while (j >= 0 && array[j] > x) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = x;
+            i = i + 1;
+        }
+
+
+        System.out.println("Sorted data: ");
+        for (int element : array
+             ) {
+            System.out.print(element + " ");
+        }
+
+    }
+
     public static void integerWriter (Scanner scanner) {
         List<Integer> list = new ArrayList<>();
         while (scanner.hasNext()) {
@@ -75,12 +110,20 @@ public class Main {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Sorter(scanner);
+
         if (args[0].equals("-dataType")) {
-            switch (args[1]) {
-                case "line" -> lineWriter(scanner);
-                case "long" -> integerWriter(scanner);
-                default -> wordWriter(scanner);
+            if (args.length > 2) {
+                Sorter(scanner);
+            } else {
+                switch (args[1]) {
+                    case "line" -> lineWriter(scanner);
+                    case "long" -> integerWriter(scanner);
+                    default -> wordWriter(scanner);
+                }
             }
+        } else if (args[0].equals("-sortIntegers")) {
+            Sorter(scanner);
         }
 
     }
